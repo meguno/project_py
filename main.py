f2b5b86551a437647ec5466b0ea9571ab7e61f2a
Semyon Urlik <urliksemen2007@gmail.com>
2025-08-15 13:49:34 +0300

    add file menu

--- main.py
+++ main.py
@@ -1,37 +1,29 @@
+from menu import Menu
 import pygame
 pygame.init()
 
-#clock = pygame.time.Clock()
+# clock = pygame.time.Clock()
 
 screen = pygame.display.set_mode((700, 700))
 pygame.display.set_caption("Tetris")
-#icon = pygame.image.load('pictures/tetris.png')
-#pygame.display.set_icon(icon)
+icon = pygame.image.load('pictures/tetris.png')
+pygame.display.set_icon(icon)
 
 bg = pygame.image.load('pictures/back.png')
 
 
-from shapes import Block
+menu = Menu(screen, 700, 700)
+project_running = True
+game_running = False
 
-block = Block(0, 0, (255, 0, 0), 40)
-
-# from field import Field
-# field = Field(400, 400, 40)
-# field.add_block(block)
-
-
-run_program = True
-while run_program:
-
-    for i in pygame.event.get():
-        if i.type == pygame.QUIT:
-            run_program = False
-
-    screen.blit(bg, (0, 0))
-
-    block.draw(screen)
-    
-    pygame.display.update()
-
-
-pygame.quit()
+while project_running:
+    if not game_running:
+        menu.draw()
+        action = menu.handle_events()
+        if action == "Start Game":
+            game_running = True
+        elif action == "Quit":
+            project_running = False
+            pygame.quit()
+    else:
+        pass
